f5c4e172bbac49f468eec52731d3ff860c618137
absaGH <78029486+absaGH@users.noreply.github.com>
2022-02-10 02:40:33 +0300

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,7 @@
+import Queue from 'bull';
+import fs from 'fs';
+import imageThumbnail from 'image-thumbnail';
+import { ObjectId } from 'mongodb';
+import dbClient from './utils/db';
+
+const fileQueue = Queue('fileQueue');
