d77107fa7ac2bed99954bfbac8baa8d5531fc78d
absaGH <78029486+absaGH@users.noreply.github.com>
2022-02-10 02:41:19 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -5,3 +5,15 @@ import { ObjectId } from 'mongodb';
 import dbClient from './utils/db';
 
 const fileQueue = Queue('fileQueue');
+
+fileQueue.process(async (job, done) => {
+  const { fileId, userId } = job.data;
+  if (!fileId) done(new Error('Missing fileId'));
+  if (!userId) done(new Error('Missing userId'));
+
+  const requestedFile = await dbClient.files.findOne({
+    _id: ObjectId(fileId),
+    userId: ObjectId(userId),
+  });
+
+  if (!requestedFile) done(new Error('File not found'));
